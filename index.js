18261b67a8298931843199b22ffbc63e13916d7a
MilkBrother <thisiswuhailong@gmail.com>
2017-12-05 15:48:09 +0800

    upload

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import Modal from './src/Modal.js';
-
+import Drawer from './src/Drawer.js';
 
 module.exports = {
     get Modal() {
         return require( './src/Modal' ).default;
+    },
+    get Drawer() {
+        return require( './src/Drawer' ).default;
     }
 }
